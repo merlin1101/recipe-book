b59e64c73ad138207aada1b6900181645fb8f5e5
merlin1101 <merlinponnachan11@gmail.com>
2023-04-11 09:48:43 +0400

    Updates

---
+++ precache-manifest.5e784204aff41c1d39d6660eb162bac7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "aad39f88b99f30388fa7",
+    "url": "/recipe-book/static/css/main.9c13e43b.chunk.css"
+  },
+  {
+    "revision": "aad39f88b99f30388fa7",
+    "url": "/recipe-book/static/js/main.aad39f88.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/recipe-book/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/recipe-book/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "bb97f0f56db05aed8050",
+    "url": "/recipe-book/static/js/runtime~main.bb97f0f5.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/recipe-book/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "d8408f8087a912185f632a290174f9fc",
+    "url": "/recipe-book/index.html"
+  }
+];
